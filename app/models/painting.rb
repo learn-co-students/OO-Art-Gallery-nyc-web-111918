c0609d3db87f38e6734931ccd1bf60872027d832
github username <github email address>
2018-11-27 21:14:22 -0500

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,25 @@
 class Painting
 
   attr_reader :title, :price
+  attr_accessor :gallery, :artist
 
   @@all = []
+  def self.all
+    @@all
+  end
 
   def initialize(title, price)
     @title = title
     @price = price
+    @gallery = nil
+    @artist = nil
+
+    @@all << self
   end
+
+  def self.total_price
+    @@all.reduce(0) {|acc, price| acc + price.price}/(@@all.count.to_f)
+  end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,17 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("John", 20)
+a2 = Artist.new("Mike", 10)
+a3 = Artist.new("Haward", 15)
+a4 = Artist.new("Chris", 6)
 
 
+g1 = Gallery.new("NY", "NYC")
+g2 = Gallery.new("NY", "NYC")
+g3 = Gallery.new("NY", "NYC")
+
+p1 = Painting.new("cold", 500)
+p2 = Painting.new("the fall", 10000)
 
 binding.pry
 
