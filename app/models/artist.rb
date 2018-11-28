b59f9e6baf6b508f1519544acc7ef1bc56de5d01
Tonyjlum <tonyjlum@gmail.com>
2018-11-28 13:02:48 -0500

    Done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,59 @@
 class Artist
-
   attr_reader :name, :years_experience
-
+  @@all = []
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select { |painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map {|painting| painting.gallery}.uniq
+  end
+
+  def cities
+    galleries.map {|gallery| gallery.city}
+  end
+
+  def self.total_experience
+    all.map {|artist| artist.years_experience}.reduce(:+)
+  end
+
+  def self.artist_to_number_of_painting_hash
+    hsh = Hash.new()
+    all.each { |artist| hsh[artist] = artist.paintings.count }
+    hsh
+  end
+
+  #refractor-each should be their own method
+    #have a method that sets a hash of artist and prolific
+      #number of yearsofexp/painting
+    #sort that hash by value
+    #pull out the last value of the hash, and return the "key" which should be the artist.
+
+  def self.most_prolific
+    most_prolific = nil
+    proficiency = 0
+    artist_to_number_of_painting_hash.each do |artist, count|
+      most_prolific = artist if most_prolific.nil?
+      next if count == 0
+      if (artist.years_experience/count).to_f < proficiency
+        most_prolific = artist
+        proficiency = (artist.years_experience/count).to_f
+      end
+    end
+    most_prolific
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,23 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("Tony", 1)
+a2 = Artist.new("Jose", 10)
+a3 = Artist.new("Terry", 26)
+a4 = Artist.new("Van Gogh", 20)
+
+g1 = Gallery.new("Moma", "New York, New York")
+g2 = Gallery.new("Queens Library", "Flushing, New York")
+g3 = Gallery.new("My basement..", "Ridgewood, New York")
+
+#(title, price, artist, gallery)
+p1 = Painting.new("The okay wave", 260, a2, g1)
+p2 = Painting.new("Much Abstract", 5600, a2, g2)
+p3 = Painting.new("Stick figure 002", 5, a1, g3)
+p4 = Painting.new("Stick figure 003", 5, a1, g3)
+p4 = Painting.new("Starry Night", 30000000, a4, g3)
+p5 = Painting.new("My second Work", 5000, a2, g2)
+
+
 
 
 
