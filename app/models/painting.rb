87ebf25cd527c64fba725bf73880cea0618a12ca
satyasinha94 <satyasinha94@gmail.com>
2018-11-28 01:09:52 -0500

    Done

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,23 @@
 class Painting
-
-  attr_reader :title, :price
-
   @@all = []
 
-  def initialize(title, price)
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    total = self.all.map {|object| object.price}.reduce(:+)
+    total / self.all.length.to_f
+  end
+
+  attr_reader :title, :price, :artist, :gallery
+
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+    @@all <<  self
   end
 
 end
