4a272049fd9af7a15650e3a296625c97450aa2a4
josh-sea <joshua.cocciardi@gmail.com>
2018-11-28 21:55:29 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,55 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
+  def self.all
+    @@all
+  end
+
+  def self.total_experience
+      @@all.reduce(0) do |sum, element|
+        sum + element.years_experience
+      end
+    end
+
+    def self.most_prolific
+      hash = {}
+      Artist.all.map do |artist_inst|
+        # binding.pry
+        hash[artist_inst] = ((artist_inst.paintings.length + 0.0) / artist_inst.years_experience)
+      end
+      hash.sort_by{|k,v| v }[0][0]
+
+    end
+
+
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def paintings
+    Painting.all.select do |painting_inst|
+      # binding.pry
+      painting_inst.artist == self
+    end
+  end
+
+  def galleries
+    paintings.map do |painting_inst|
+      painting_inst.gallery
+    end
+  end
+
+  def cities
+    galleries.map do |gallery_inst|
+      gallery_inst.city
+    end
+  end
+
+    def create_painting(title, price, gallery)
+      Painting.new(title,price,self,gallery)
+  end
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,24 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist, :gallery
 
   @@all = []
-
-  def initialize(title, price)
-    @title = title
-    @price = price
+  def self.all
+    @@all
   end
 
+  def initialize(title, price, artist, gallery)
+    @title = title
+    @price = price
+    @artist = artist
+    @gallery = gallery
+    @@all << self
+  end
+
+  def self.total_price
+    @@all.reduce(0) do |sum, element|
+      sum + element.price
+    end
+  end
+  
 end
