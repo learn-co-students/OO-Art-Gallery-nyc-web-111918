40d51cbe5155ce23f3e35b2c9f73cc6f9cbfb151
Matt Doering <ablueplatypus21@gmail.com>
2018-11-28 19:47:01 -0500

    done

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,32 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    total_price = self.all.reduce(0) {|acc, el| acc += el.price }
+    total_price.to_f / self.all.size
+  end
+
+  # total = 0
+  # array.each do |ele|
+  #   total += ele
+  # end
+
+  # arr.reduce(0) { |acc, ele| acc += ele }
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+
+
+    @@all << self
   end
 
 end
